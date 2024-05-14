2e73d798f2c4d7cd144320435fc2996075d04010
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-05-14 17:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", async function (event) {
+	event.preventDefault();
+	const text = document.getElementById("text").value;
+	const time= parseInt(document.getElementById("delay").value,10);
+	function del(ms) {
+		return new Promise(resolve => setTimeout(resolve,ms))
+	}
+	if(isNaN(time)||time<0){
+		alert('Please enter a valid delay in milliseconds.');
+    return;
+	}
+	await del(time);
+	document.getElementById('output').innerText=text;
+	
+})
